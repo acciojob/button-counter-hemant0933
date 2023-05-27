e91346b01b7b8f0f64291cffe1e1eba33053ae7a
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-05-27 21:06:04 +0530

    submitting code-Sat May 27 2023 21:06:03 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,13 @@
-
-<<<<<<< HEAD
 import React, { useState } from "react";
-=======
-import React, {useState} from "react";
->>>>>>> 44d066a5aa8fb196b508dc27795a74930720e00c
 import './../styles/App.css';
 import BtnComponent from "./BtnComponent";
+
 const App = () => {
   const [count, setCount] = useState(0);
-<<<<<<< HEAD
+
   return (
     <div>
-      
-=======
-  
-  return (
-    <div>
-    <p>Button clicked {count} times</p><br/><br/>
-    <button onClick={() => setCount(count + 1)}>Click me</button>
->>>>>>> 44d066a5aa8fb196b508dc27795a74930720e00c
+    <BtnComponent/>
     </div>
   )
 }
